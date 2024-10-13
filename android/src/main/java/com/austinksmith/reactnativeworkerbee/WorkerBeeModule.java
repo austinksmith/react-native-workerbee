feb4e57b25213dd28e6ac67d182c0fc7c03512f2
Austin Smith <austin@asmithdev.com>
2024-10-12 23:25:51 -0400

    Clean up module

--- android/src/main/java/com/austinksmith/reactnativeworkerbee/WorkerBeeModule.java
+++ android/src/main/java/com/austinksmith/reactnativeworkerbee/WorkerBeeModule.java
@@ -8,36 +8,35 @@ import com.facebook.react.turbomodule.core.CallInvokerHolderImpl;
 import com.facebook.react.turbomodule.core.interfaces.CallInvokerHolder;
 import com.facebook.jni.HybridData;
 
-@ReactModule(name = WorkerBeeModule.NAME)
+@ReactModule(name = WorkerBeeModule.NAME) // Annotation that makes this module available to React Native
 public class WorkerBeeModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "WorkerBeeModule";
+    public static final String NAME = "WorkerBeeModule"; // The name of the module
 
-    private final HybridData mHybridData;
+    private final HybridData mHybridData; // To store hybrid data
 
     public WorkerBeeModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        mHybridData = initHybrid(reactContext.getJavaScriptContextHolder().get(), (CallInvokerHolderImpl) reactContext.getCatalystInstance().getJSCallInvokerHolder());
+        // Initialize HybridData using the JavaScript context and the call invoker holder
+        mHybridData = initHybrid(reactContext.getJavaScriptContextHolder().get(),
+                                  (CallInvokerHolderImpl) reactContext.getCatalystInstance().getJSCallInvokerHolder());
     }
 
     @Override
     public String getName() {
-        return NAME;
+        return NAME; // Return the name of the module
     }
 
+    // Declare native methods to be implemented in C++
     private native HybridData initHybrid(long jsContext, CallInvokerHolder jsCallInvokerHolder);
-
     private native void installJSIBindings();
 
     @ReactMethod(isBlockingSynchronousMethod = true)
     public boolean install() {
-        installJSIBindings();
-        return true;
+        installJSIBindings(); // Call the native method to install JSI bindings
+        return true; // Return true to indicate success
     }
 
-    // Native methods to be implemented in C++
-    public native long createWorker(String scriptURL);
-    public native void postMessage(long workerId, String message);
-    public native void terminateWorker(long workerId);
-} workerbee {
-  
+    public native long createWorker(String scriptURL); // Native method to create a worker
+    public native void postMessage(long workerId, String message); // Method to post a message to the worker
+    public native void terminateWorker(long workerId); // Method to terminate the worker
 }
