d5e18519656f0a8ebbb3579f07bf92c9d439205d
Austin Smith <austin@asmithdev.com>
2024-10-16 09:13:49 -0400

    It almost works

--- android/src/main/java/com/austinksmith/reactnativeworkerbee/WorkerBeeModule.java
+++ android/src/main/java/com/austinksmith/reactnativeworkerbee/WorkerBeeModule.java
@@ -1,5 +1,7 @@
 package com.austinksmith.reactnativeworkerbee;
 
+import android.util.Log;
+
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
@@ -8,35 +10,42 @@ import com.facebook.react.turbomodule.core.CallInvokerHolderImpl;
 import com.facebook.react.turbomodule.core.interfaces.CallInvokerHolder;
 import com.facebook.jni.HybridData;
 
-@ReactModule(name = WorkerBeeModule.NAME) // Annotation that makes this module available to React Native
+@ReactModule(name = WorkerBeeModule.NAME)
 public class WorkerBeeModule extends ReactContextBaseJavaModule {
-    public static final String NAME = "WorkerBeeModule"; // The name of the module
+    public static final String NAME = "WorkerBeeModule";
 
-    private final HybridData mHybridData; // To store hybrid data
+    static {
+        System.loadLibrary("workerbee-jni");
+    }
+
+    private final HybridData mHybridData;
 
     public WorkerBeeModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        // Initialize HybridData using the JavaScript context and the call invoker holder
         mHybridData = initHybrid(reactContext.getJavaScriptContextHolder().get(),
-                                  (CallInvokerHolderImpl) reactContext.getCatalystInstance().getJSCallInvokerHolder());
+                                 (CallInvokerHolderImpl) reactContext.getCatalystInstance().getJSCallInvokerHolder());
     }
 
     @Override
     public String getName() {
-        return NAME; // Return the name of the module
+        return NAME;
     }
 
-    // Declare native methods to be implemented in C++
     private native HybridData initHybrid(long jsContext, CallInvokerHolder jsCallInvokerHolder);
     private native void installJSIBindings();
 
     @ReactMethod(isBlockingSynchronousMethod = true)
     public boolean install() {
-        installJSIBindings(); // Call the native method to install JSI bindings
-        return true; // Return true to indicate success
+        try {
+            installJSIBindings();
+            return true;
+        } catch (Exception e) {
+            Log.e(NAME, "Failed to install JSI bindings", e);
+            return false;
+        }
     }
 
-    public native long createWorker(String scriptURL); // Native method to create a worker
-    public native void postMessage(long workerId, String message); // Method to post a message to the worker
-    public native void terminateWorker(long workerId); // Method to terminate the worker
+    public native long createWorker(String scriptURL);
+    public native void postMessage(long workerId, String message);
+    public native void terminateWorker(long workerId);
 }
